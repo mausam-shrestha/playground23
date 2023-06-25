5e8bd11f0e4d0321fa02abd4f9748aa5b2706925
Mausam <46900712+mausam-shrestha@users.noreply.github.com>
2023-06-25 17:06:33 +0100

    Adds a simple validation and removes uint parsing

---
+++ Playground.Core/Services/Validator.cs
@@ -0,0 +1,19 @@
+﻿namespace Playground.Core.Services
+{
+    public class Validator : IValidator
+    {
+        private readonly int _minimumInput;
+        private readonly int _maximumInput;
+
+        public Validator(int minimumInput, int maximumInput)
+        {
+            _minimumInput = minimumInput;
+            _maximumInput = maximumInput;
+        }
+
+        public bool IsValid()
+        {
+            return (_minimumInput > 0 && _maximumInput > 0);
+        }
+    }
+}
