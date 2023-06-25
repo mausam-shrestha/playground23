855633d7e1f0e91828da53576084df13f72b3612
Mausam <46900712+mausam-shrestha@users.noreply.github.com>
2023-06-25 16:11:12 +0100

    Implements rules for the game which ensures all tests are passing

--- Playground.Core/Services/FizzBuzzGame.cs
+++ Playground.Core/Services/FizzBuzzGame.cs
@@ -4,7 +4,22 @@
     {
         public string GetOutput(uint input)
         {
-            return string.Empty;
+            if (input % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+
+            if (input % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (input % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return input.ToString();
         }
     }
 }
